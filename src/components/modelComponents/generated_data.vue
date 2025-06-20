d4d039607346bd5aa23d20b515c29d7f571a5287
jerry <85928945+Jeries-Qanaza@users.noreply.github.com>
2025-06-20 22:12:28 +0300

    Update generated_data.vue

--- src/components/modelComponents/generated_data.vue
+++ src/components/modelComponents/generated_data.vue
@@ -3,12 +3,13 @@
     <h1>Generated Data</h1>
     <!-- Loop through meals and pass each meal as a prop to GeneratedMeal -->
     <GeneratedMeal
-      v-for="(meal, index) in meals"
-      :key="index"
-      :mealName="meal.mealName"
-      :steps="meal.steps"
-      :image="meal.image"
-       />
+  v-for="(meal, index) in meals"
+  :key="index"
+  :mealName="meal.mealName"
+  :description="meal.description"
+  :steps="meal.steps"
+  :image="meal.image"
+/>
   </div>
 </template>
 
