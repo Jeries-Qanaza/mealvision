48d7193cdba5303af5fc078512a0155876d6a943
Yohaiabi <yohai141@gmail.com>
2025-07-02 01:02:51 +0300

    Install new style pack - PrimeVue

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+// PrimeVue Import
+import PrimeVue from "primevue/config";
+
+// Paths for PrimeVue v3+
+import "primevue/resources/themes/lara-light-indigo/theme.css"; // A popular default theme
+import "primevue/resources/primevue.min.css"; // Core CSS
+import "primeicons/primeicons.css"; // Icons
+
+const app = createApp(App);
+app.use(router);
+
+// Use PrimeVue with ripple effect enabled
+app.use(PrimeVue, { ripple: true });
+app.mount("#app");
