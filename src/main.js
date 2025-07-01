a0c68005fea3e67eb255b977106c230c30ed8f6f
Yohaiabi <yohai141@gmail.com>
2025-07-02 02:05:33 +0300

    Install new style pack - PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+// PrimeVue v4 with a preset theme
 import PrimeVue from "primevue/config";
-import Aura from "@primevue/themes/aura";
+import Lara from "@primeuix/themes/lara";
 
 const app = createApp(App);
-app.use(router, PrimeVue, {
+
+app.use(router);
+app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: Lara,
   },
 });
+
 app.mount("#app");
