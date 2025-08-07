7063a0fab3cfd3e3068bfba31bc3c565405ca02c
Yohaiabi <yohai141@gmail.com>
2025-08-07 14:31:25 +0300

    Remove Primevue Theme

--- src/main.js
+++ src/main.js
@@ -2,17 +2,4 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-// PrimeVue v4 with a preset theme
-import PrimeVue from "primevue/config";
-import Lara from "@primeuix/themes/lara";
-
-const app = createApp(App);
-
-app.use(router);
-app.use(PrimeVue, {
-  theme: {
-    preset: Lara,
-  },
-});
-
-app.mount("#app");
+createApp(App).use(router).mount("#app");
